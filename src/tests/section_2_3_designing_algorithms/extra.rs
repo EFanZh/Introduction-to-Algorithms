4d33ae3f18725289471ab2572ccc77c2cb27458b
EFanZh <efanzh@gmail.com>
2018-11-11 22:00:51 +0800

    Add another merge sort implementation that minimize copying elements

--- src/tests/section_2_3_designing_algorithms/extra.rs
+++ src/tests/section_2_3_designing_algorithms/extra.rs
@@ -1,6 +1,7 @@
 use super::{
     super::{
-        super::section_2_3_designing_algorithms::extra::merge_sort_allocate_once, utilities::run_all_sorting_tests,
+        super::section_2_3_designing_algorithms::extra::{merge_sort_allocate_once, merge_sort_allocate_once_2},
+        utilities::run_all_sorting_tests,
     },
     Bencher,
 };
@@ -8,4 +9,9 @@ use super::{
 #[bench]
 fn test_merge_sort_allocate_once(b: &mut Bencher) {
     b.iter(|| run_all_sorting_tests(merge_sort_allocate_once));
+}
+
+#[bench]
+fn test_merge_sort_allocate_once_2(b: &mut Bencher) {
+    b.iter(|| run_all_sorting_tests(merge_sort_allocate_once_2));
 }
