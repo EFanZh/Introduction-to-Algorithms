16fc421e4f1bf600178b4aa8db73c268149dc9c4
EFanZh <efanzh@gmail.com>
2019-01-27 19:40:41 +0800

    Add brute force implementation of Find-Maximum-Subarray

---
+++ src/section_4_1_the_maximum_subarray_problem/exercises/exercise_4_1_3.rs
@@ -0,0 +1,50 @@
+use num_traits::Zero;
+use std::ops::Add;
+
+// Find-Maximum-Subarray-Brute-Force(A)
+//
+//  1.  max-left = 0
+//  2.  max-right = 0
+//  3.  max-sum = -∞
+//  4.  for i = 1 to A.Length
+//  5.      sum = 0
+//  6.      for j = i to n
+//  7.          sum = sum + A[j]
+//  8.          if sum > max-sum
+//  9.              max-left = i
+// 10.              max-right = j
+// 11.              max-sum = sum
+// 12.  return (max-left, max-right, max-sum)
+
+pub fn find_maximum_subarray_brute_force<T: Add + Clone + Ord + Zero>(a: &[T]) -> (usize, usize, T) {
+    let mut max_left = 0usize;
+    let mut max_right = 0usize;
+    let mut max_sum = a[0].clone();
+
+    for i in 0..a.len() {
+        let mut sum = T::zero();
+
+        for (j, a_j) in a.iter().enumerate().skip(i) {
+            sum = sum + a_j.clone();
+
+            if sum > max_sum {
+                max_left = i;
+                max_right = j;
+                max_sum = sum.clone();
+            }
+        }
+    }
+
+    (max_left, max_right + 1, max_sum)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::super::utilities::run_find_maximum_subarray_tests;
+    use super::find_maximum_subarray_brute_force;
+
+    #[test]
+    fn test_find_maximum_subarray_brute_force() {
+        run_find_maximum_subarray_tests(find_maximum_subarray_brute_force);
+    }
+}
---
+++ src/section_4_1_the_maximum_subarray_problem/utilities.rs
@@ -0,0 +1,54 @@
+pub fn run_find_maximum_subarray_tests<F: Fn(&[i32]) -> (usize, usize, i32)>(f: F) {
+    fn run_test<T: AsRef<[i32]>, F: Fn(&[i32]) -> (usize, usize, i32)>(f: F, a: T, expected: i32) {
+        let a_ref = a.as_ref();
+
+        assert!(!a_ref.is_empty());
+
+        let (left, right, sum) = f(a_ref);
+
+        assert!(right > left);
+
+        assert_eq!(sum, a_ref[left..right].iter().sum());
+        assert_eq!(sum, expected);
+    }
+
+    run_test(&f, [0], 0);
+    run_test(&f, [-3], -3);
+    run_test(&f, [4], 4);
+
+    run_test(&f, [-3, -1], -1);
+    run_test(&f, [-1, -3], -1);
+    run_test(&f, [-3, 0], 0);
+    run_test(&f, [-3, 3], 3);
+    run_test(&f, [0, -3], 0);
+    run_test(&f, [0, 0], 0);
+    run_test(&f, [0, 3], 3);
+    run_test(&f, [3, -1], 3);
+    run_test(&f, [3, 0], 3);
+    run_test(&f, [3, 4], 7);
+    run_test(&f, [4, 3], 7);
+
+    run_test(&f, [-7, -3, -1], -1);
+    run_test(&f, [-7, -1, -3], -1);
+    run_test(&f, [-3, -7, -1], -1);
+    run_test(&f, [-3, -1, -7], -1);
+    run_test(&f, [-1, -7, -3], -1);
+    run_test(&f, [-1, -3, -7], -1);
+    run_test(&f, [-7, -3, 1], 1);
+    run_test(&f, [-7, 1, -3], 1);
+    run_test(&f, [-3, -7, 1], 1);
+    run_test(&f, [-3, 1, -7], 1);
+    run_test(&f, [1, -7, -3], 1);
+    run_test(&f, [1, -3, -7], 1);
+    run_test(&f, [-7, 3, 1], 4);
+    run_test(&f, [-7, 1, 3], 4);
+    run_test(&f, [3, -7, 1], 3);
+    run_test(&f, [3, 1, -7], 4);
+    run_test(&f, [1, -7, 3], 3);
+    run_test(&f, [1, 3, -7], 4);
+
+    run_test(&f, [2, -1, 4], 5);
+    run_test(&f, [2, 1, 4], 7);
+    run_test(&f, [2, 0, 4], 6);
+    run_test(&f, [2, 4, 0], 6);
+}
