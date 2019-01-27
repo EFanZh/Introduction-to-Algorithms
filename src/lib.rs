8312e6a78f5239b8c9298f0538d1bf585b432889
EFanZh <efanzh@gmail.com>
2019-01-27 16:04:54 +0800

    Add algorithms from section 4.1

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 pub mod section_2_1_insertion_sort;
 pub mod section_2_2_analyzing_algorithms;
 pub mod section_2_3_designing_algorithms;
+pub mod section_4_1_the_maximum_subarray_problem;
 mod utilities;
 
 #[cfg(test)]
