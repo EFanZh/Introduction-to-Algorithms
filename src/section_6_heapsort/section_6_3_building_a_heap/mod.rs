335fe4cc5e343c195ba9da98321c8eddd5dc143a
EFanZh <efanzh@gmail.com>
2019-04-27 22:12:20 +0800

    Implement Build-Min-Heap

--- src/section_6_heapsort/section_6_3_building_a_heap/mod.rs
+++ src/section_6_heapsort/section_6_3_building_a_heap/mod.rs
@@ -1,3 +1,4 @@
+use super::section_6_2_maintaining_the_heap_property::exercises::exercise_6_2_2::min_heapify;
 use super::section_6_2_maintaining_the_heap_property::max_heapify;
 
 // Build-Max-Heap(A)
@@ -12,13 +13,23 @@ pub fn build_max_heap<T: Ord>(a: &mut [T]) {
     }
 }
 
+pub fn build_min_heap<T: Ord>(a: &mut [T]) {
+    for i in (0..a.len() / 2).rev() {
+        min_heapify(a, i);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::super::section_6_1_heaps::parent;
-    use super::build_max_heap;
+    use super::{build_max_heap, build_min_heap};
 
     fn is_max_heap<T: Ord>(a: &[T]) -> bool {
         a.iter().enumerate().skip(1).all(|(i, v)| v <= &a[parent(i)])
+    }
+
+    fn is_min_heap<T: Ord>(a: &[T]) -> bool {
+        a.iter().enumerate().skip(1).all(|(i, v)| v >= &a[parent(i)])
     }
 
     #[test]
@@ -30,6 +41,49 @@ mod tests {
             build_max_heap(&mut a_1);
 
             assert!(is_max_heap(&a_1));
+
+            a_1.sort_unstable();
+            a_2.sort_unstable();
+
+            assert_eq!(a_1, a_2);
+        }
+
+        run_single_test(&[]);
+
+        run_single_test(&[0]);
+
+        run_single_test(&[0, 0]);
+        run_single_test(&[0, 1]);
+        run_single_test(&[1, 0]);
+
+        run_single_test(&[0, 0, 0]);
+        run_single_test(&[0, 0, 1]);
+        run_single_test(&[0, 1, 0]);
+        run_single_test(&[0, 1, 1]);
+        run_single_test(&[0, 1, 2]);
+        run_single_test(&[0, 2, 1]);
+        run_single_test(&[1, 0, 0]);
+        run_single_test(&[1, 0, 1]);
+        run_single_test(&[1, 0, 2]);
+        run_single_test(&[1, 1, 0]);
+        run_single_test(&[1, 2, 0]);
+        run_single_test(&[2, 0, 1]);
+        run_single_test(&[2, 1, 0]);
+
+        // Test case from CLRS.
+
+        run_single_test(&[4, 1, 3, 2, 16, 9, 10, 14, 8, 7]);
+    }
+
+    #[test]
+    fn test_build_min_heap() {
+        fn run_single_test(a: &[i32]) {
+            let mut a_1 = a.to_vec();
+            let mut a_2 = a_1.clone();
+
+            build_min_heap(&mut a_1);
+
+            assert!(is_min_heap(&a_1));
 
             a_1.sort_unstable();
             a_2.sort_unstable();
