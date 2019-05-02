6a9fea256c738ded8aaf065884aaa00ce8023df5
EFanZh <efanzh@gmail.com>
2019-05-02 22:33:20 +0800

    Move `KeyValuePair` to utilities

--- src/utilities.rs
+++ src/utilities.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::{cmp::Reverse, mem::replace};
 
 #[derive(Clone, PartialEq, PartialOrd)]
@@ -22,5 +23,36 @@ impl<T> Default for MaxSentinel<T> {
 impl<T> From<T> for MaxSentinel<T> {
     fn from(v: T) -> Self {
         MaxSentinel(Reverse(Some(Reverse(v))))
+    }
+}
+
+pub struct KeyValuePair<K, V> {
+    pub key: K,
+    pub value: V,
+}
+
+impl<K, V> KeyValuePair<K, V> {
+    pub fn new(key: K, value: V) -> Self {
+        KeyValuePair { key, value }
+    }
+}
+
+impl<K: PartialEq, V> PartialEq for KeyValuePair<K, V> {
+    fn eq(&self, other: &Self) -> bool {
+        self.key.eq(&other.key)
+    }
+}
+
+impl<K: Eq, V> Eq for KeyValuePair<K, V> {}
+
+impl<K: PartialOrd, V> PartialOrd for KeyValuePair<K, V> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.key.partial_cmp(&other.key)
+    }
+}
+
+impl<K: Ord, V> Ord for KeyValuePair<K, V> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.key.cmp(&other.key)
     }
 }
