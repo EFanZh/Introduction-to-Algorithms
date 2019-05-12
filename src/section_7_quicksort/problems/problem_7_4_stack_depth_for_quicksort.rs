e39375273e4ea77492f24358a2fbf6780f67cda6
EFanZh <efanzh@gmail.com>
2019-05-12 14:48:41 +0800

    Implement `tail_recursive_quicksort_limited_stack_depth`

--- src/section_7_quicksort/problems/problem_7_4_stack_depth_for_quicksort.rs
+++ src/section_7_quicksort/problems/problem_7_4_stack_depth_for_quicksort.rs
@@ -20,13 +20,40 @@ pub fn tail_recursive_quicksort<T: Ord>(a: &mut [T], mut p: usize, r: usize) {
     }
 }
 
+pub fn tail_recursive_quicksort_limited_stack_depth<T: Ord>(a: &mut [T], mut p: usize, mut r: usize) {
+    while r - p > 1 {
+        // Partition
+
+        let q = partition(a, p, r);
+
+        if q - p < r - (p + 1) {
+            // Sort left subarray.
+
+            tail_recursive_quicksort(a, p, q);
+
+            p = q + 1;
+        } else {
+            // Sort right subarray.
+
+            tail_recursive_quicksort(a, p + 1, r);
+
+            r = q;
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::super::super::super::test_utilities::run_all_sorting_tests;
-    use super::tail_recursive_quicksort;
+    use super::{tail_recursive_quicksort, tail_recursive_quicksort_limited_stack_depth};
 
     #[test]
     fn test_tail_recursive_quicksort() {
         run_all_sorting_tests(|a| tail_recursive_quicksort(a, 0, a.len()));
+    }
+
+    #[test]
+    fn test_tail_recursive_quicksort_limited_stack_depth() {
+        run_all_sorting_tests(|a| tail_recursive_quicksort_limited_stack_depth(a, 0, a.len()));
     }
 }
