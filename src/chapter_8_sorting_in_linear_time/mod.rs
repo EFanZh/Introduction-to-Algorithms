16ee0c0450adca3095bbae2fb548b8560150855f
EFanZh <efanzh@gmail.com>
2019-05-18 17:24:14 +0800

    Implement radix sort

--- src/chapter_8_sorting_in_linear_time/mod.rs
+++ src/chapter_8_sorting_in_linear_time/mod.rs
@@ -1,1 +1,2 @@
 pub mod section_8_2_counting_sort;
+pub mod section_8_3_radix_sort;
---
+++ src/chapter_8_sorting_in_linear_time/section_8_3_radix_sort/mod.rs
@@ -0,0 +1,45 @@
+// Radix-Sort(A, d)
+// 1  for i = 1 to d
+// 2      use a stable sort to sort array A on digit i
+
+pub fn radix_sort<T, S: FnMut(&mut [T], usize)>(a: &mut [T], d: usize, mut stable_sorter: S) {
+    for i in (0..d).rev() {
+        stable_sorter(a, i);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::super::test_utilities::{assign_vec, assign_vec_from_iter};
+    use super::super::section_8_2_counting_sort::extra::counting_sort_by_key;
+    use super::radix_sort;
+    use rand::{thread_rng, Rng};
+
+    #[test]
+    fn test_radix_sort() {
+        let mut array_1 = Vec::new();
+        let mut array_2 = Vec::new();
+        let mut rng = thread_rng();
+
+        for n in 0usize..10 {
+            for k in 1..4 {
+                for _ in 0..2usize.pow(n as _) {
+                    assign_vec_from_iter(
+                        &mut array_1,
+                        (0..n).map(|_| (0..k).map(|_| rng.gen_range(0, n)).collect::<Box<_>>()),
+                    );
+
+                    assign_vec(&mut array_2, &array_1);
+
+                    radix_sort(&mut array_1, k, |a, i| {
+                        counting_sort_by_key(a, Default::default(), |x| x[i]);
+                    });
+
+                    array_2.sort_unstable();
+
+                    assert_eq!(array_1, array_2);
+                }
+            }
+        }
+    }
+}
