b9dcd38277fd07b987d835c07db50827bb3b5a40
EFanZh <efanzh@gmail.com>
2019-05-18 10:52:22 +0800

    Implement algorithm from exercise 8.2-3

--- src/chapter_8_sorting_in_linear_time/section_8_2_counting_sort/exercises/exercise_8_2_3.rs
+++ src/chapter_8_sorting_in_linear_time/section_8_2_counting_sort/exercises/exercise_8_2_3.rs
@@ -1,21 +1,4 @@
-pub mod exercises;
-
-// Counting-Sort(A, B, k)
-//
-//  1  let C[0‥k] be a new array
-//  2  for i = 0 to k
-//  3      C[i] = 0
-//  4  for j = 1 to A.length
-//  5      C[A[j]] = C[A[j]] + 1
-//  6  // C[i] now contains the number of elements equal to i.
-//  7  for i = 1 to k
-//  8      C[i] = C[i] + C[i - 1]
-//  9  // C[i] now contains the number of elements less than or equal to i.
-// 10  for j = A.length downto 1
-// 11      B[C[A[j]]] = A[j]
-// 12      C[A[j]] = C[A[j]] - 1
-
-pub fn counting_sort(a: &[usize], b: &mut [usize], k: usize) {
+pub fn modified_counting_sort(a: &[usize], b: &mut [usize], k: usize) {
     let mut c = vec![0; k];
 
     for x in a {
@@ -30,7 +13,7 @@ pub fn counting_sort(a: &[usize], b: &mut [usize], k: usize) {
 
     // C[i] now contains the number of elements less than or equal to i.
 
-    for x in a.iter().rev() {
+    for x in a {
         b[c[*x] - 1] = *x;
 
         c[*x] -= 1;
@@ -39,12 +22,12 @@ pub fn counting_sort(a: &[usize], b: &mut [usize], k: usize) {
 
 #[cfg(test)]
 mod tests {
-    use super::super::super::test_utilities::assign_vec_from_iter;
-    use super::counting_sort;
+    use super::super::super::super::super::test_utilities::assign_vec_from_iter;
+    use super::modified_counting_sort;
     use rand::{thread_rng, Rng};
 
     #[test]
-    fn test_counting_sort() {
+    fn test_modified_counting_sort() {
         let mut a = Vec::new();
         let mut b = Vec::new();
         let mut rng = thread_rng();
@@ -54,7 +37,7 @@ mod tests {
                 assign_vec_from_iter(&mut a, (0..n).map(|_| rng.gen_range(0, n)));
                 b.resize(n, 0);
 
-                counting_sort(&a, &mut b, n);
+                modified_counting_sort(&a, &mut b, n);
 
                 a.sort_unstable();
 
