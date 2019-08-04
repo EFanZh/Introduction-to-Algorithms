b50329c48e882932e80eabb79f4b044a3a8c8103
EFanZh <efanzh@gmail.com>
2019-08-04 13:54:31 +0800

    Add exercise 11.3-2

--- src/chapter_11_hash_tables/mod.rs
+++ src/chapter_11_hash_tables/mod.rs
@@ -1,2 +1,3 @@
 pub mod section_11_1_direct_address_tables;
 pub mod section_11_2_hash_tables;
+pub mod section_11_3_hash_functions;
---
+++ src/chapter_11_hash_tables/section_11_3_hash_functions/exercises/exercise_11_3_2.rs
@@ -0,0 +1,31 @@
+pub fn hash_string(s: &[u8], m: u32) -> u32 {
+    let mut result = 0u64;
+    let m = u64::from(m);
+
+    for c in s.iter().map(|c| u64::from(*c)) {
+        result = (result * 128 + c) % m;
+    }
+
+    result as _
+}
+
+#[cfg(test)]
+mod tests {
+    use super::hash_string;
+
+    #[test]
+    fn test_hash_string() {
+        assert_eq!(hash_string(&[], 1), 0);
+        assert_eq!(hash_string(&[], 2), 0);
+        assert_eq!(hash_string(&[3], 1), 0);
+        assert_eq!(hash_string(&[3], 2), 1);
+        assert_eq!(hash_string(&[3, 5, 7], 1), 0);
+        assert_eq!(hash_string(&[3, 5, 7], 2), 1);
+        assert_eq!(hash_string(&[3, 5, 7], 3), 2);
+        assert_eq!(hash_string(&[3, 5, 7], 4), 3);
+        assert_eq!(hash_string(&[3, 5, 7], 5), 4);
+        assert_eq!(hash_string(&[3, 5, 7], 6), 5);
+        assert_eq!(hash_string(&[3, 5, 7], 7), 1);
+        assert_eq!(hash_string(&[3, 5, 7], 8), 7);
+    }
+}
