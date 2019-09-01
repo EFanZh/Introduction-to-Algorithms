2aa04528cbbd8c57ef43a13e357e9ff8ae632780
EFanZh <efanzh@gmail.com>
2019-09-01 20:27:00 +0800

    Prepare to implement insertion on red-black tree

--- src/chapter_13_red_black_trees/section_13_1_properties_of_red_black_trees/mod.rs
+++ src/chapter_13_red_black_trees/section_13_1_properties_of_red_black_trees/mod.rs
@@ -1,7 +1,66 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
 #[derive(PartialEq, Eq, Debug)]
-pub struct RedBlackTreeNode<T> {
-    pub is_red: bool,
-    pub key: T,
-    pub left: Option<Box<Self>>,
-    pub right: Option<Box<Self>>,
+pub enum Color {
+    Red,
+    Black,
 }
+
+#[derive(Debug)]
+pub struct RedBlackTreeNode<T> {
+    pub color: Color,
+    pub key: T,
+    pub left: Option<Rc<RefCell<Self>>>,
+    pub right: Option<Rc<RefCell<Self>>>,
+    pub p: Weak<RefCell<Self>>,
+}
+
+impl<T> RedBlackTreeNode<T> {
+    pub fn new(
+        color: Color,
+        key: T,
+        left: Option<Rc<RefCell<Self>>>,
+        right: Option<Rc<RefCell<Self>>>,
+    ) -> Rc<RefCell<Self>> {
+        let result = Rc::new(RefCell::new(Self {
+            color,
+            key,
+            left,
+            right,
+            p: Weak::new(),
+        }));
+
+        {
+            let result_ref = result.borrow();
+
+            if let Some(left) = &result_ref.left {
+                left.borrow_mut().p = Rc::downgrade(&result);
+            }
+
+            if let Some(right) = &result_ref.right {
+                right.borrow_mut().p = Rc::downgrade(&result);
+            }
+        }
+
+        result
+    }
+
+    pub fn new_leaf(color: Color, key: T) -> Rc<Self> {
+        Rc::new(Self {
+            color,
+            key,
+            left: None,
+            right: None,
+            p: Weak::new(),
+        })
+    }
+}
+
+impl<T: PartialEq> PartialEq for RedBlackTreeNode<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.color == other.color && self.key == other.key && self.left == other.left && self.right == other.right
+    }
+}
+
+impl<T: Eq> Eq for RedBlackTreeNode<T> {}
--- src/chapter_13_red_black_trees/section_13_2_rotations/exercises/exercise_13_2_1.rs
+++ src/chapter_13_red_black_trees/section_13_2_rotations/exercises/exercise_13_2_1.rs
@@ -1,29 +1,56 @@
 use super::super::super::section_13_1_properties_of_red_black_trees::RedBlackTreeNode;
+use std::cell::RefCell;
+use std::mem;
+use std::rc::{Rc, Weak};
 
-pub fn right_rotate<T>(y: &mut Option<Box<RedBlackTreeNode<T>>>) {
-    let mut y_node = y.take().unwrap();
-    let mut x_node = y_node.left.take().unwrap();
+pub fn right_rotate<T>(y: &mut Option<Rc<RefCell<RedBlackTreeNode<T>>>>) {
+    // Break y from parent.
 
-    y_node.left = x_node.right.take();
-    x_node.right = Some(y_node);
+    let y_rc = y.take().unwrap();
+    let mut y_ref = y_rc.borrow_mut();
+    let p_weak = mem::replace(&mut y_ref.p, Weak::new());
 
-    *y = Some(x_node);
+    // Break x from y.
+
+    let x_rc = y_ref.left.take().unwrap();
+    let mut x_ref = x_rc.borrow_mut();
+    let y_weak = mem::replace(&mut x_ref.p, Weak::new());
+
+    // Attach x.right to y.left.
+
+    if let Some(x_right) = x_ref.right.take() {
+        x_right.borrow_mut().p = y_weak;
+        y_ref.left = Some(x_right);
+    }
+
+    // Attach y to x.right;
+
+    y_ref.p = Rc::downgrade(&x_rc);
+
+    drop(y_ref);
+
+    x_ref.right = Some(y_rc);
+
+    // Attach x to parent.
+
+    x_ref.p = p_weak;
+
+    drop(x_ref);
+
+    *y = Some(x_rc);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::super::super::super::section_13_1_properties_of_red_black_trees::RedBlackTreeNode;
+    use super::super::super::super::section_13_1_properties_of_red_black_trees::{Color, RedBlackTreeNode};
     use super::right_rotate;
+    use std::cell::RefCell;
+    use std::rc::Rc;
 
-    type Tree<T> = Option<Box<RedBlackTreeNode<T>>>;
+    type Tree<T> = Option<Rc<RefCell<RedBlackTreeNode<T>>>>;
 
     fn make_node<T>(key: T, left: Tree<T>, right: Tree<T>) -> Tree<T> {
-        Some(Box::new(RedBlackTreeNode {
-            is_red: false,
-            key,
-            left,
-            right,
-        }))
+        Some(RedBlackTreeNode::new(Color::Black, key, left, right))
     }
 
     #[test]
