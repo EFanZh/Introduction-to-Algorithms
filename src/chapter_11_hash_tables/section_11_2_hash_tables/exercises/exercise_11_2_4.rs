6e5ce5dd2c852c3cea7d07d84744c7a792cc0705
EFanZh <efanzh@gmail.com>
2019-09-08 12:04:58 +0800

    Increase test coverage
    Writing tests for hash tables are hard!

--- src/chapter_11_hash_tables/section_11_2_hash_tables/exercises/exercise_11_2_4.rs
+++ src/chapter_11_hash_tables/section_11_2_hash_tables/exercises/exercise_11_2_4.rs
@@ -200,37 +200,10 @@ mod tests {
         Inspect(Vec<i32>, Vec<i32>),
     }
 
-    #[test]
-    fn test_hash_table() {
-        use HashTableOperation::{Delete, Insert, Inspect};
+    use HashTableOperation::{Delete, Insert, Inspect};
 
-        let test_case = vec![
-            Inspect(vec![], vec![2, 3, 5, 7, 11]),
-            Insert(2),
-            Inspect(vec![2], vec![3, 5, 7, 11]),
-            Insert(3),
-            Inspect(vec![2, 3], vec![5, 7, 11]),
-            Insert(5),
-            Inspect(vec![2, 3, 5], vec![7, 11]),
-            Insert(7),
-            Inspect(vec![2, 3, 5, 7], vec![11]),
-            Insert(11),
-            Inspect(vec![2, 3, 5, 7, 11], vec![]),
-            Delete(3),
-            Inspect(vec![2, 5, 7, 11], vec![3]),
-            Delete(7),
-            Inspect(vec![2, 5, 11], vec![3, 7]),
-            Delete(5),
-            Inspect(vec![2, 11], vec![3, 5, 7]),
-            Delete(2),
-            Inspect(vec![11], vec![2, 3, 5, 7]),
-            Delete(11),
-            Inspect(vec![], vec![2, 3, 5, 7, 11]),
-        ];
-
-        let mut hash_table = HashTable::with_capacity(5);
-
-        for operation in test_case {
+    fn run_hash_table_tests(mut hash_table: HashTable<i32>, operations: Vec<HashTableOperation>) {
+        for operation in operations {
             match operation {
                 Insert(value) => hash_table.insert(value),
                 Delete(value) => hash_table.delete(&value),
@@ -245,5 +218,149 @@ mod tests {
                 }
             }
         }
+    }
+
+    #[test]
+    fn test_hash_table_1() {
+        run_hash_table_tests(
+            HashTable::with_capacity(1),
+            vec![
+                Inspect(vec![], vec![2]),
+                Insert(2),
+                Inspect(vec![2], vec![]),
+                Delete(2),
+                Inspect(vec![], vec![2]),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_hash_table_2() {
+        run_hash_table_tests(
+            HashTable::with_capacity(2),
+            vec![
+                Inspect(vec![], vec![2, 3]),
+                Insert(2),
+                Inspect(vec![2], vec![3]),
+                Insert(3),
+                Inspect(vec![2, 3], vec![]),
+                Delete(2),
+                Inspect(vec![3], vec![2]),
+                Delete(3),
+                Inspect(vec![], vec![2, 3]),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_hash_table_3() {
+        run_hash_table_tests(
+            HashTable::with_capacity(3),
+            vec![
+                Inspect(vec![], vec![2, 3, 5]),
+                Insert(2),
+                Inspect(vec![2], vec![3, 5]),
+                Insert(3),
+                Inspect(vec![2, 3], vec![5]),
+                Insert(5),
+                Inspect(vec![2, 3, 5], vec![]),
+                Delete(2),
+                Inspect(vec![3, 5], vec![2]),
+                Delete(5),
+                Inspect(vec![3], vec![2, 5]),
+                Delete(3),
+                Inspect(vec![], vec![2, 3, 5]),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_hash_table_4() {
+        run_hash_table_tests(
+            HashTable::with_capacity(4),
+            vec![
+                Inspect(vec![], vec![2, 3, 5, 7]),
+                Insert(2),
+                Inspect(vec![2], vec![3, 5, 7]),
+                Insert(3),
+                Inspect(vec![2, 3], vec![5, 7]),
+                Insert(5),
+                Inspect(vec![2, 3, 5], vec![7]),
+                Insert(7),
+                Inspect(vec![2, 3, 5, 7], vec![]),
+                Delete(7),
+                Inspect(vec![2, 3, 5], vec![7]),
+                Delete(2),
+                Inspect(vec![3, 5], vec![2, 7]),
+                Delete(5),
+                Inspect(vec![3], vec![2, 5, 7]),
+                Delete(3),
+                Inspect(vec![], vec![2, 3, 5, 7]),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_hash_table_5() {
+        run_hash_table_tests(
+            HashTable::with_capacity(5),
+            vec![
+                Inspect(vec![], vec![2, 3, 5, 7, 11]),
+                Insert(2),
+                Inspect(vec![2], vec![3, 5, 7, 11]),
+                Insert(3),
+                Inspect(vec![2, 3], vec![5, 7, 11]),
+                Insert(5),
+                Inspect(vec![2, 3, 5], vec![7, 11]),
+                Insert(7),
+                Inspect(vec![2, 3, 5, 7], vec![11]),
+                Insert(11),
+                Inspect(vec![2, 3, 5, 7, 11], vec![]),
+                Delete(3),
+                Inspect(vec![2, 5, 7, 11], vec![3]),
+                Delete(7),
+                Inspect(vec![2, 5, 11], vec![3, 7]),
+                Delete(5),
+                Inspect(vec![2, 11], vec![3, 5, 7]),
+                Delete(2),
+                Inspect(vec![11], vec![2, 3, 5, 7]),
+                Delete(11),
+                Inspect(vec![], vec![2, 3, 5, 7, 11]),
+            ],
+        );
+    }
+
+    #[test]
+    fn test_hash_table_6() {
+        run_hash_table_tests(
+            HashTable::with_capacity(6),
+            vec![
+                Inspect(vec![], vec![2, 3, 5, 7, 11, 13]),
+                Insert(2),
+                Inspect(vec![2], vec![3, 5, 7, 11, 13]),
+                Insert(3),
+                Inspect(vec![2, 3], vec![5, 7, 11, 13]),
+                Insert(5),
+                Inspect(vec![2, 3, 5], vec![7, 11, 13]),
+                Insert(7),
+                Inspect(vec![2, 3, 5, 7], vec![11, 13]),
+                Insert(11),
+                Inspect(vec![2, 3, 5, 7, 11], vec![13]),
+                Insert(13),
+                Inspect(vec![2, 3, 5, 7, 11, 13], vec![]),
+                Delete(7),
+                Inspect(vec![2, 3, 5, 11, 13], vec![7]),
+                Delete(2),
+                Inspect(vec![3, 5, 11, 13], vec![2, 7]),
+                Delete(5),
+                Inspect(vec![3, 11, 13], vec![2, 5, 7]),
+                Delete(11),
+                Inspect(vec![3, 13], vec![2, 5, 7, 11]),
+                Delete(13),
+                Inspect(vec![3], vec![2, 5, 7, 11, 13]),
+                Delete(3),
+                Inspect(vec![], vec![2, 3, 5, 7, 11, 13]),
+            ],
+        );
     }
 }
