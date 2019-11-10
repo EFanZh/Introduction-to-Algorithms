bfbaae0c34ae3ad833ce30f79ba463f048f41111
EFanZh <efanzh@gmail.com>
2019-11-10 21:11:33 +0800

    Add exercise 15.2-2

---
+++ src/chapter_15_dynamic_programming/section_15_2_matrix_chain_multiplication/exercises/exercise_15_2_2.rs
@@ -0,0 +1,50 @@
+use ndarray::{Array2, LinalgScalar};
+
+pub fn matrix_chain_multiply<T: Clone + LinalgScalar>(
+    a: &[Array2<T>],
+    s: &Array2<usize>,
+    i: usize,
+    j: usize,
+) -> Array2<T> {
+    if i + 1 == j {
+        a[i].clone()
+    } else {
+        let k = s[[i, j - 2]];
+        let lhs = matrix_chain_multiply(a, s, i, k);
+        let rhs = matrix_chain_multiply(a, s, k, j);
+
+        lhs.dot(&rhs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::super::super::matrix_chain_order;
+    use super::matrix_chain_multiply;
+    use ndarray::array;
+    use std::iter;
+
+    #[test]
+    fn test_matrix_chain_multiply() {
+        let a = [
+            array![[1, 2, 3], [4, 5, 6]],
+            array![[1, 2], [3, 4], [5, 6]],
+            array![[1, 2, 3, 4], [5, 6, 7, 8]],
+        ];
+
+        let p = iter::once(a[0].dim().0)
+            .chain(a.iter().map(|x| x.dim().1))
+            .collect::<Box<_>>();
+
+        let (_, s) = matrix_chain_order(&p);
+
+        let expected_result = array![[162, 212, 262, 312], [369, 482, 595, 708]];
+
+        assert_eq!(&matrix_chain_multiply(&a, &s, 0, a.len()), &expected_result);
+
+        assert_eq!(
+            a[1..].iter().fold(a[0].clone(), |lhs, rhs| lhs.dot(rhs)),
+            array![[162, 212, 262, 312], [369, 482, 595, 708]]
+        );
+    }
+}
--- src/chapter_15_dynamic_programming/section_15_2_matrix_chain_multiplication/exercises/mod.rs
+++ src/chapter_15_dynamic_programming/section_15_2_matrix_chain_multiplication/exercises/mod.rs
@@ -1,1 +1,2 @@
 pub mod exercise_15_2_1;
+pub mod exercise_15_2_2;
