faa76ea14fa73fc7dcaa0b8dfdf60e7f37a51986
EFanZh <efanzh@gmail.com>
2019-11-23 17:29:32 +0800

    Add exercise 15.4-4

---
+++ src/chapter_15_dynamic_programming/section_15_4_longest_common_subsequence/exercises/exercise_15_4_4.rs
@@ -0,0 +1,57 @@
+use std::mem;
+
+pub fn lcs_length_1<T: Eq>(x: &[T], y: &[T]) -> usize {
+    let (x, y) = if y.len() < x.len() { (y, x) } else { (x, y) };
+    let mut cache = vec![0; (x.len() + 1) * 2];
+    let (mut cache_1, mut cache_2) = cache.split_at_mut(x.len() + 1);
+
+    for y_j in y {
+        for (i, x_i) in x.iter().enumerate() {
+            cache_2[i + 1] = if x_i == y_j {
+                cache_1[i] + 1
+            } else {
+                cache_2[i].max(cache_1[i + 1])
+            };
+        }
+
+        mem::swap(&mut cache_1, &mut cache_2);
+    }
+
+    *cache_1.last().unwrap()
+}
+
+pub fn lcs_length_2<T: Eq>(x: &[T], y: &[T]) -> usize {
+    let (x, y) = if y.len() < x.len() { (y, x) } else { (x, y) };
+    let mut cache = vec![0; x.len() + 1];
+
+    for y_j in y {
+        let mut old_value = 0;
+
+        for (i, x_i) in x.iter().enumerate() {
+            let new_value = if x_i == y_j {
+                old_value + 1
+            } else {
+                cache[i].max(cache[i + 1])
+            };
+
+            old_value = mem::replace(&mut cache[i + 1], new_value);
+        }
+    }
+
+    *cache.last().unwrap()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{lcs_length_1, lcs_length_2};
+
+    #[test]
+    fn test_memoized_lcs_length_1() {
+        assert_eq!(lcs_length_1(b"ABCBDAB", b"BDCABA"), 4);
+    }
+
+    #[test]
+    fn test_memoized_lcs_length_2() {
+        assert_eq!(lcs_length_2(b"ABCBDAB", b"BDCABA"), 4);
+    }
+}
--- src/chapter_15_dynamic_programming/section_15_4_longest_common_subsequence/exercises/mod.rs
+++ src/chapter_15_dynamic_programming/section_15_4_longest_common_subsequence/exercises/mod.rs
@@ -1,3 +1,4 @@
 pub mod exercise_15_4_1;
 pub mod exercise_15_4_2;
 pub mod exercise_15_4_3;
+pub mod exercise_15_4_4;
