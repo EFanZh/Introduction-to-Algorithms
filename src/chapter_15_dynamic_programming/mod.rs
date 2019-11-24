02a1f567a7b358b2a44017958ebc70b5895967a7
EFanZh <efanzh@gmail.com>
2019-11-24 20:47:09 +0800

    Implement optimal binary search trees algorithm

--- src/chapter_15_dynamic_programming/mod.rs
+++ src/chapter_15_dynamic_programming/mod.rs
@@ -1,3 +1,4 @@
 pub mod section_15_1_rod_cutting;
 pub mod section_15_2_matrix_chain_multiplication;
 pub mod section_15_4_longest_common_subsequence;
+pub mod section_15_5_optimal_binary_search_trees;
