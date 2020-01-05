c6d7feaa3c9a46d4e431f5e778a802bd9d33802f
EFanZh <efanzh@gmail.com>
2020-01-05 15:10:49 +0800

    Implement Recursive-Activity-Selector algorithm

---
+++ src/chapter_16_greedy_algorithms/section_16_1_an_activity_selection_problem/mod.rs
@@ -0,0 +1,40 @@
+// Recursive-Activity-Selector(s, f, k, n)
+//
+// 1  m = k + 1
+// 2  while m ≤ n and s[m] < f[k] // find the first activity in S_k to finish
+// 3      m = m + 1
+// 4  if m ≤ n
+// 5       return {a_m} ∪ Recursive-Activity-Selector(s, f, m, n)
+// 6  else return ∅
+
+pub fn recursive_activity_selector(s: &[u64], f: &[u64], k: usize) -> Vec<usize> {
+    let last_finish_time = k.checked_sub(1).map(|i| f[i]).unwrap_or(0);
+
+    if let Some(m) = s
+        .iter()
+        .enumerate()
+        .skip(k)
+        .find_map(|(i, s_i)| if *s_i < last_finish_time { None } else { Some(i) })
+    {
+        let mut result = recursive_activity_selector(s, f, m + 1);
+
+        result.push(m);
+
+        result
+    } else {
+        Vec::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::recursive_activity_selector;
+
+    #[test]
+    fn test_recursive_activity_selector() {
+        let s = [1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12];
+        let f = [4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16];
+
+        assert_eq!(recursive_activity_selector(&s, &f, 0), [10, 7, 3, 0]);
+    }
+}
