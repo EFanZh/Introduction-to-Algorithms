22319a2b463defabf6538f433902ed08ca385138
EFanZh <efanzh@gmail.com>
2020-01-05 15:25:37 +0800

    Implement Greedy-Activity-Selector algorithm

--- src/chapter_16_greedy_algorithms/section_16_1_an_activity_selection_problem/mod.rs
+++ src/chapter_16_greedy_algorithms/section_16_1_an_activity_selection_problem/mod.rs
@@ -26,9 +26,37 @@ pub fn recursive_activity_selector(s: &[u64], f: &[u64], k: usize) -> Vec<usize>
     }
 }
 
+// Greedy-Activity-Selector(s, f)
+//
+// 1  n = s.length
+// 2  A = {a_1}
+// 3  k = 1
+// 4  for m = 2 to n
+// 5      if s[m] ≥ f[k]
+// 6          A = A ∪ {a_m}
+// 7          k = m
+// 8  return A
+
+pub fn greedy_activity_selector(s: &[u64], f: &[u64]) -> Box<[usize]> {
+    let mut last_finish_time = 0;
+
+    s.iter()
+        .enumerate()
+        .filter_map(|(i, s_i)| {
+            if *s_i >= last_finish_time {
+                last_finish_time = f[i];
+
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
-    use super::recursive_activity_selector;
+    use super::{greedy_activity_selector, recursive_activity_selector};
 
     #[test]
     fn test_recursive_activity_selector() {
@@ -36,5 +64,13 @@ mod tests {
         let f = [4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16];
 
         assert_eq!(recursive_activity_selector(&s, &f, 0), [10, 7, 3, 0]);
+    }
+
+    #[test]
+    fn test_greedy_activity_selector() {
+        let s = [1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12];
+        let f = [4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16];
+
+        assert_eq!(*greedy_activity_selector(&s, &f), [0, 3, 7, 10]);
     }
 }
