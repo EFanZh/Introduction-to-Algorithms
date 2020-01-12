1341a5a1157ae71bb933536ceaa1c0fba352fe66
EFanZh <efanzh@gmail.com>
2020-01-12 21:57:56 +0800

    Add exercise 16.2-5

---
+++ src/chapter_16_greedy_algorithms/section_16_2_elements_of_the_greedy_strategy/exercises/exercise_16_2_5.rs
@@ -0,0 +1,33 @@
+// Assume the points are sorted in increasing order.
+
+pub fn cover_points(mut points: &[f64]) -> Box<[f64]> {
+    let mut result = Vec::new();
+
+    while let Some((&first_point, rest_points)) = points.split_first() {
+        result.push(first_point);
+
+        let interval_end = first_point + 1.0;
+
+        if let Some(next) = rest_points.iter().position(|x| *x > interval_end) {
+            points = &rest_points[next..];
+        } else {
+            break;
+        }
+    }
+
+    result.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::cover_points;
+
+    #[test]
+    fn test_cover_points() {
+        let points = [0.46, 2.08, 2.24, 3.27, 4.23, 4.58, 6.20, 7.63, 8.79, 9.62];
+        let result = cover_points(&points);
+        let exprected_result = [0.46, 2.08, 3.27, 4.58, 6.20, 7.63, 8.79];
+
+        assert_eq!(*result, exprected_result);
+    }
+}
--- src/chapter_16_greedy_algorithms/section_16_2_elements_of_the_greedy_strategy/exercises/mod.rs
+++ src/chapter_16_greedy_algorithms/section_16_2_elements_of_the_greedy_strategy/exercises/mod.rs
@@ -1,2 +1,3 @@
 pub mod exercise_16_2_2;
 pub mod exercise_16_2_4;
+pub mod exercise_16_2_5;
