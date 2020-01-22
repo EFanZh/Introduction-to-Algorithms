1ecd750e9be48bc44fcc8a91a453d23c228e93f3
EFanZh <efanzh@gmail.com>
2020-01-22 21:52:48 +0800

    Add exercise 16.2-7

---
+++ src/chapter_16_greedy_algorithms/section_16_2_elements_of_the_greedy_strategy/exercises/exercise_16_2_7.rs
@@ -0,0 +1,24 @@
+pub fn reorder_sets<T: Ord>(a: &mut [T], b: &mut [T]) {
+    assert_eq!(a.len(), b.len());
+
+    a.sort_unstable();
+    b.sort_unstable();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::reorder_sets;
+
+    #[test]
+    fn test_reorder_sets() {
+        // It is really not necessary to test this on.
+
+        let mut a = [5, 7, 1, 8];
+        let mut b = [5, 2, 3, 9];
+
+        reorder_sets(&mut a, &mut b);
+
+        assert_eq!(a, [1, 5, 7, 8]);
+        assert_eq!(b, [2, 3, 5, 9]);
+    }
+}
--- src/chapter_16_greedy_algorithms/section_16_2_elements_of_the_greedy_strategy/exercises/mod.rs
+++ src/chapter_16_greedy_algorithms/section_16_2_elements_of_the_greedy_strategy/exercises/mod.rs
@@ -2,3 +2,4 @@ pub mod exercise_16_2_2;
 pub mod exercise_16_2_4;
 pub mod exercise_16_2_5;
 pub mod exercise_16_2_6;
+pub mod exercise_16_2_7;
