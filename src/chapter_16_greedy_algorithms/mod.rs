7d534a49f964555983d9450f993e23bc2f6c56d8
EFanZh <efanzh@gmail.com>
2020-02-02 16:36:20 +0800

    Add problem 16-1: Coin changing

--- src/chapter_16_greedy_algorithms/mod.rs
+++ src/chapter_16_greedy_algorithms/mod.rs
@@ -1,3 +1,4 @@
+pub mod problems;
 pub mod section_16_1_an_activity_selection_problem;
 pub mod section_16_2_elements_of_the_greedy_strategy;
 pub mod section_16_3_huffman_codes;
---
+++ src/chapter_16_greedy_algorithms/problems/problem_16_1_coin_changing/question_a.rs
@@ -0,0 +1,82 @@
+#[derive(PartialEq, Eq, Debug)]
+pub struct Coins {
+    quarters: usize,
+    dimes: usize,
+    nickels: usize,
+    pennies: usize,
+}
+
+const QUARTER_CENTS: usize = 25;
+const DIME_CENTS: usize = 10;
+const NICKEL_CENTS: usize = 5;
+const PENNY_CENTS: usize = 1;
+
+pub fn change_coins(mut n: usize) -> Coins {
+    let mut result = Coins {
+        quarters: 0,
+        dimes: 0,
+        nickels: 0,
+        pennies: 0,
+    };
+
+    for (target, value) in &mut [
+        (&mut result.quarters, QUARTER_CENTS),
+        (&mut result.dimes, DIME_CENTS),
+        (&mut result.nickels, NICKEL_CENTS),
+        (&mut result.pennies, PENNY_CENTS),
+    ] {
+        if n == 0 {
+            break;
+        } else {
+            **target = n / *value;
+
+            n -= *value * **target;
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::change_coins;
+
+    #[test]
+    fn test_change_coins() {
+        let test_cases = [
+            (0, [0, 0, 0, 0]),
+            (1, [0, 0, 0, 1]),
+            (2, [0, 0, 0, 2]),
+            (5, [0, 0, 1, 0]),
+            (6, [0, 0, 1, 1]),
+            (7, [0, 0, 1, 2]),
+            (10, [0, 1, 0, 0]),
+            (11, [0, 1, 0, 1]),
+            (12, [0, 1, 0, 2]),
+            (15, [0, 1, 1, 0]),
+            (16, [0, 1, 1, 1]),
+            (17, [0, 1, 1, 2]),
+            (25, [1, 0, 0, 0]),
+            (26, [1, 0, 0, 1]),
+            (27, [1, 0, 0, 2]),
+            (30, [1, 0, 1, 0]),
+            (31, [1, 0, 1, 1]),
+            (32, [1, 0, 1, 2]),
+            (35, [1, 1, 0, 0]),
+            (36, [1, 1, 0, 1]),
+            (37, [1, 1, 0, 2]),
+            (40, [1, 1, 1, 0]),
+            (41, [1, 1, 1, 1]),
+            (42, [1, 1, 1, 2]),
+        ];
+
+        for (n, [quarters, dimes, nickels, pennies]) in test_cases.iter().copied() {
+            let result = change_coins(n);
+
+            assert_eq!(result.quarters, quarters);
+            assert_eq!(result.dimes, dimes);
+            assert_eq!(result.nickels, nickels);
+            assert_eq!(result.pennies, pennies);
+        }
+    }
+}
