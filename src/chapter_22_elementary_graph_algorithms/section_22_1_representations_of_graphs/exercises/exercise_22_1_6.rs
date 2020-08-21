94c9ef6a2e653d5e9180e2271e92cbd1a06987f6
EFanZh <efanzh@gmail.com>
2020-08-21 22:07:47 +0800

    Add exercise 22.1-6

---
+++ src/chapter_22_elementary_graph_algorithms/section_22_1_representations_of_graphs/exercises/exercise_22_1_6.rs
@@ -0,0 +1,55 @@
+use ndarray::{s, Array2, Axis};
+
+pub fn has_universal_sink(matrix: &Array2<bool>) -> bool {
+    let n = matrix.nrows();
+    let mut row = 0;
+    let mut column = n - 1;
+
+    while row != column {
+        if matrix[(row, column)] {
+            row += 1;
+        } else {
+            column -= 1;
+        }
+    }
+
+    let row_view = matrix.index_axis(Axis(0), row);
+    let column_view = matrix.index_axis(Axis(1), column);
+
+    !row_view.iter().any(|x| *x)
+        && column_view.slice(s![..column]).iter().all(|x| *x)
+        && column_view.slice(s![column + 1..]).iter().all(|x| *x)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::has_universal_sink;
+
+    #[test]
+    fn test_has_universal_sink() {
+        let test_cases = [
+            (
+                ndarray::arr2(&[
+                    [false, true, false, false],
+                    [false, false, true, false],
+                    [false, false, false, true],
+                    [false, false, false, false],
+                ]),
+                false,
+            ),
+            (
+                ndarray::arr2(&[
+                    [false, true, false, false],
+                    [false, false, true, false],
+                    [false, true, false, true],
+                    [false, true, false, false],
+                ]),
+                false,
+            ),
+        ];
+
+        for (matrix, expected) in test_cases.iter().cloned() {
+            assert_eq!(has_universal_sink(&matrix), expected);
+        }
+    }
+}
