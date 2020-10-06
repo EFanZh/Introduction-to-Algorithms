0b481bfd2cb2f88f3bc8b2183d271e138940c71c
EFanZh <efanzh@gmail.com>
2020-10-06 15:27:10 +0800

    Add exercise 22.3-12

---
+++ src/chapter_22_elementary_graph_algorithms/section_22_3_depth_first_search/exercises/exercise_22_3_12.rs
@@ -0,0 +1,59 @@
+fn dfs_visit(graph: &[Vec<usize>], node: usize, visited: &mut [bool], cc: usize, result: &mut [usize]) {
+    result[node] = cc;
+
+    for &next in &graph[node] {
+        if let value @ false = &mut visited[next] {
+            *value = true;
+
+            dfs_visit(graph, next, visited, cc, result);
+        }
+    }
+}
+
+pub fn get_connected_components(graph: &[Vec<usize>]) -> Vec<usize> {
+    let mut cc = 0;
+    let mut visited = vec![false; graph.len()];
+    let mut result = vec![0; graph.len()];
+
+    for node in 0..graph.len() {
+        if let value @ false = &mut visited[node] {
+            *value = true;
+
+            dfs_visit(graph, node, &mut visited, cc, &mut result);
+
+            cc += 1;
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_connected_components;
+
+    #[test]
+    fn test_get_connected_components() {
+        let test_cases = [
+            (
+                &[
+                    &[1_usize, 3] as &[usize],
+                    &[0, 3, 4],
+                    &[4, 5],
+                    &[0, 1, 4],
+                    &[1, 2, 3],
+                    &[2, 5],
+                ] as &[&[usize]],
+                &[0, 0, 0, 0, 0, 0],
+            ),
+            (&[&[], &[2], &[1], &[4, 5], &[3], &[3]], &[0, 1, 1, 2, 2, 2]),
+        ];
+
+        for (graph, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                get_connected_components(graph.iter().map(|node| node.to_vec()).collect::<Box<_>>().as_ref()),
+                expected
+            );
+        }
+    }
+}
