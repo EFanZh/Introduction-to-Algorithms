09c0d75c6fd609276fa5efae232b7843fb09c440
EFanZh <efanzh@gmail.com>
2020-10-07 21:07:19 +0800

    Add exercise 22.4-5

---
+++ src/chapter_22_elementary_graph_algorithms/section_22_4_topological_sort/exercises/exercise_22_4_5.rs
@@ -0,0 +1,79 @@
+pub fn topological_sort(graph: &[Vec<usize>]) -> Option<Vec<usize>> {
+    let mut in_degrees = vec![0; graph.len()];
+
+    for nexts in graph.iter() {
+        for &next in nexts {
+            in_degrees[next] += 1;
+        }
+    }
+
+    let mut queue = in_degrees
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &indegree)| if indegree == 0 { Some(i) } else { None })
+        .collect::<Vec<_>>();
+
+    let mut result = Vec::new();
+
+    while let Some(node) = queue.pop() {
+        result.push(node);
+
+        for &next in &graph[node] {
+            let in_degree = &mut in_degrees[next];
+
+            if *in_degree == 1 {
+                queue.push(next);
+            } else {
+                *in_degree -= 1;
+            }
+        }
+    }
+
+    if result.len() == graph.len() {
+        Some(result)
+    } else {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::topological_sort;
+    use std::collections::HashSet;
+
+    #[test]
+    fn test_has_cycle() {
+        let test_cases: &[(&[&[usize]], bool)] = &[
+            (&[&[1], &[]], true),
+            (&[&[1, 2], &[], &[], &[1, 2]], true),
+            (&[&[]], true),
+            (&[&[1], &[0]], false),
+            (&[&[1], &[0], &[1]], false),
+        ];
+
+        for (graph, can_finish) in test_cases.iter().copied() {
+            let graph = graph.iter().map(|nexts| nexts.to_vec()).collect::<Box<_>>();
+            let result = topological_sort(&graph);
+
+            if can_finish {
+                let result = result.unwrap();
+
+                assert_eq!(result.iter().copied().collect::<HashSet<_>>().len(), graph.len());
+
+                let mut indices = vec![0; graph.len()];
+
+                for (i, node) in result.into_iter().enumerate() {
+                    indices[node] = i;
+                }
+
+                for (node, nexts) in graph.iter().enumerate() {
+                    for &next in nexts {
+                        assert!(indices[node] < indices[next]);
+                    }
+                }
+            } else {
+                assert!(result.is_none());
+            }
+        }
+    }
+}
