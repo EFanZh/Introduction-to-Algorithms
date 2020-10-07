5d21737619e7d45d80b7aec6fc1b46f368fce404
EFanZh <efanzh@gmail.com>
2020-10-07 15:33:14 +0800

    Add exercise 22.4-2

---
+++ src/chapter_22_elementary_graph_algorithms/section_22_4_topological_sort/exercises/exercise_22_4_2.rs
@@ -0,0 +1,46 @@
+fn dfs(graph: &[Vec<usize>], from: usize, to: usize, cache: &mut Vec<Option<usize>>) -> usize {
+    if let Some(result) = cache[from] {
+        result
+    } else {
+        let result = if from == graph.len() - 1 {
+            1
+        } else {
+            graph[from].iter().map(|&next| dfs(graph, next, to, cache)).sum()
+        };
+
+        cache[from] = Some(result);
+
+        result
+    }
+}
+
+pub fn get_number_of_paths(graph: &[Vec<usize>], s: usize, t: usize) -> usize {
+    dfs(&graph, s, t, &mut vec![None; graph.len()])
+}
+
+#[cfg(test)]
+mod tests {
+    use super::get_number_of_paths;
+
+    #[test]
+    fn test_get_number_of_paths() {
+        let test_cases = [
+            ((&[&[1_usize, 2] as &[usize], &[3], &[3], &[]] as &[&[_]], 0, 3), 2),
+            ((&[&[4, 3, 1], &[3, 2, 4], &[3], &[4], &[]], 0, 3), 5),
+            ((&[&[1], &[]], 0, 0), 1),
+            ((&[&[1, 2, 3], &[2], &[3], &[]], 0, 3), 3),
+            ((&[&[1, 3], &[2], &[3], &[]], 0, 3), 2),
+        ];
+
+        for ((graph, s, t), expected) in test_cases.iter().copied() {
+            assert_eq!(
+                get_number_of_paths(
+                    graph.iter().map(|nexts| nexts.to_vec()).collect::<Box<_>>().as_ref(),
+                    s,
+                    t
+                ),
+                expected
+            );
+        }
+    }
+}
