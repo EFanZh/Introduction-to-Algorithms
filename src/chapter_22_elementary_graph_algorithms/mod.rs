abe3ebe73dc77aaab365271d5e31c5fc6c517c45
EFanZh <efanzh@gmail.com>
2020-10-08 21:17:33 +0800

    Implement strongly connected components algorithm

--- src/chapter_22_elementary_graph_algorithms/mod.rs
+++ src/chapter_22_elementary_graph_algorithms/mod.rs
@@ -2,3 +2,4 @@ pub mod section_22_1_representations_of_graphs;
 pub mod section_22_2_breadth_first_search;
 pub mod section_22_3_depth_first_search;
 pub mod section_22_4_topological_sort;
+pub mod section_22_5_strongly_connected_components;
---
+++ src/chapter_22_elementary_graph_algorithms/section_22_5_strongly_connected_components/mod.rs
@@ -0,0 +1,102 @@
+fn dfs_1(graph: &[Vec<usize>], nodes: impl IntoIterator<Item = usize>, visited: &mut [bool], result: &mut Vec<usize>) {
+    for node in nodes {
+        if let visited_value @ false = &mut visited[node] {
+            *visited_value = true;
+
+            dfs_1(graph, graph[node].iter().copied(), visited, result);
+
+            result.push(node);
+        }
+    }
+}
+
+fn dfs_2(graph: &[Vec<usize>], node: usize, visited: &mut [bool], result: &mut Vec<usize>) {
+    result.push(node);
+
+    for &next in &graph[node] {
+        if let visited_value @ false = &mut visited[next] {
+            *visited_value = true;
+
+            dfs_2(graph, next, visited, result);
+        }
+    }
+}
+
+// Strongly-Connected-Components(G)
+//
+// 1  call DFS(G) to compute finishing times u.f for each vertex u
+// 2  compute G^T
+// 3  call DFS(G^T), but in the main loop of DFS, consider the vertices in order of decreasing u.f (as computed in line 1)
+// 4  output the vertices of each tree in the depth-first forest formed in line 3 as a separate strongly connected component
+
+pub fn strongly_connected_components(g: &[Vec<usize>]) -> Vec<Vec<usize>> {
+    // First DFS.
+
+    let mut visited = vec![false; g.len()];
+    let mut nodes = Vec::with_capacity(g.len());
+
+    dfs_1(g, 0..g.len(), &mut visited, &mut nodes);
+
+    // Transpose graph.
+
+    let mut transposed_graph = vec![Vec::new(); g.len()];
+
+    for (node, nexts) in g.iter().enumerate() {
+        for &next in nexts {
+            transposed_graph[next].push(node);
+        }
+    }
+
+    // Second DFS.
+
+    // TODO: Use `slice::fill` when it is stablized.
+
+    for value in &mut visited {
+        *value = false;
+    }
+
+    let mut result = Vec::new();
+
+    for node in nodes.into_iter().rev() {
+        if let visited_value @ false = &mut visited[node] {
+            *visited_value = true;
+
+            let mut component = Vec::new();
+
+            dfs_2(&transposed_graph, node, &mut visited, &mut component);
+
+            result.push(component)
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::strongly_connected_components;
+
+    #[test]
+    fn test_strongly_connected_components() {
+        let test_cases = &[(
+            &[
+                &[1_usize] as &[usize],
+                &[2, 4, 5],
+                &[3, 6],
+                &[2, 7],
+                &[0, 5],
+                &[6],
+                &[5, 7],
+                &[7],
+            ] as &[&[_]],
+            &[&[0_usize, 4, 1] as &[usize], &[2, 3], &[6, 5], &[7]],
+        )];
+
+        for (g, expected) in test_cases.iter().copied() {
+            assert_eq!(
+                strongly_connected_components(g.iter().map(|nexts| nexts.to_vec()).collect::<Box<_>>().as_ref()),
+                expected
+            );
+        }
+    }
+}
